7127c1126d1a9cf18c8813a7f183d849195f5ac6
Eli Fine <ejfine@gmail.com>
2025-01-30 04:17:03 +0000

    copier prettier

--- biotasker_app/nuxt.config.ts
+++ biotasker_app/nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui',        '@toyopilgrim/nuxt-alt-auth',
-    '@pinia/nuxt'],
+  modules: ["@nuxt/ui", "@toyopilgrim/nuxt-alt-auth", "@pinia/nuxt"],
   experimental: { appManifest: false }, // https://github.com/nuxt/nuxt/issues/30461#issuecomment-2572616714
-  vite: { // this seems to be explicitly needed when in a devcontainer in order for hot reloading to work
+  vite: {
+    // this seems to be explicitly needed when in a devcontainer in order for hot reloading to work
     server: {
       watch: {
         usePolling: true,
       },
     },
   },
-})
+});
